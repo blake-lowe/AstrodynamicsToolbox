213f87d202e0013260a97f694380151e6b02e310
Blake Lowe <9019960+dragonfyre23@users.noreply.github.com>
2023-10-25 00:45:58 -0400

    Deleted redundant files

--- rth2eph.m
+++
@@ -1,9 +0,0 @@
-function [out] = rth2eph(in, TA)
-    % Rotation matrix from orbit frame to rotating frame
-    R = [cos(TA) sin(TA) 0;
-        -sin(TA) cos(TA) 0;
-        0 0 1];
-
-    out = R'*in;
-end
-
